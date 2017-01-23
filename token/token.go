2650ca9c8642b0b86a5f8d9eac42b9bd3a260b26
Keisuke KITA <kei.kita2501@gmail.com>
2017-01-23 21:37:37 +0900

    Until 1.3 (p.22)

--- token/token.go
+++ token/token.go
@@ -8,7 +8,7 @@ type Token struct {
 }
 
 const (
-	ILEEGAL = "ILLEGAL"
+	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
 	// Identifiers + literals
@@ -32,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
