8e45d56539fe0e3b0b198d765dc9439cf1ba26cf
Keisuke KITA <kei.kita2501@gmail.com>
2017-02-11 16:18:30 +0900

    p.117 PREFIX EXPRESSIONS の前まで

---
+++ evaluator/evaluator.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"github.com/kitak-sandbox/writing_an_interpreter_in_go/ast"
+	"github.com/kitak-sandbox/writing_an_interpreter_in_go/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	// Statements
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+		// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	} else {
+		return FALSE
+	}
+}
