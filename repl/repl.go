0eb2a7ddf7aa6a77990770d28254df3d69207ff9
Keisuke KITA <kei.kita2501@gmail.com>
2017-02-15 07:45:37 +0900

    p.140 まで読んだ。次は 3.10 から

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/kitak-sandbox/writing_an_interpreter_in_go/evaluator"
 	"github.com/kitak-sandbox/writing_an_interpreter_in_go/lexer"
+	"github.com/kitak-sandbox/writing_an_interpreter_in_go/object"
 	"github.com/kitak-sandbox/writing_an_interpreter_in_go/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
